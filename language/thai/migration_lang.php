4e6f5650e45132bd1fd75b39b59ba41989c46ce8
ninearif <ninearif@hotmail.com>
2018-09-07 11:24:16 +0700

    Thai - Correct Spelling, Fix space/tab based on official english (v. 3.1.9)

--- language/thai/migration_lang.php
+++ language/thai/migration_lang.php
@@ -9,11 +9,11 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['migration_none_found']		= 'ไม่พบการโยกย้าย';
-$lang['migration_not_found']		= 'ไม่พบข้อมูลการโยกย้าย หมายเลขเวอร์ชั่น: %s.';
-$lang['migration_sequence_gap']		= 'มีช่องว่างในลำดับการโยกย้ายที่อยู่ใกล้หมายเลขรุ่นคือ: %s.';
-$lang['migration_multiple_version']	= 'มีการโยกย้ายหลายรายการที่มีหมายเลขรุ่นเดียวกัน: %s.';
-$lang['migration_class_doesnt_exist']	= 'ไม่พบ Migration Class "%s"';
-$lang['migration_missing_up_method']	= 'Migration class "%s" ไม่มีเมธอด "up"';
-$lang['migration_missing_down_method']	= 'Migration class "%s" ไม่มีเมธอด "down"';
-$lang['migration_invalid_filename']	= 'การโยกย้าย "%s" มีไฟล์ที่ไม่ถูกต้อง';
+$lang['migration_none_found'] = 'ไม่พบการ Migration';
+$lang['migration_not_found'] = 'ไม่พบข้อมูล Migration หมายเลขเวอร์ชัน: %s.';
+$lang['migration_sequence_gap'] = 'มีช่องว่างในลำดับของ Migration ที่อยู่ใกล้หมายเลขรุ่นคือ: %s.';
+$lang['migration_multiple_version'] = 'มีการ Migration หลายรายการที่มีหมายเลขรุ่นซ้ำกัน: %s.';
+$lang['migration_class_doesnt_exist'] = 'ไม่พบ Migration Class "%s"';
+$lang['migration_missing_up_method'] = 'Migration class "%s" ไม่มีเมธอด "up"';
+$lang['migration_missing_down_method'] = 'Migration class "%s" ไม่มีเมธอด "down"';
+$lang['migration_invalid_filename'] = 'Migration "%s" มีชื่อไฟล์ที่ไม่ถูกต้อง';
--- language/thai/unit_test_lang.php
+++ language/thai/unit_test_lang.php
@@ -9,22 +9,22 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['ut_test_name']		= 'ชื่อกากรทดสอบ';
-$lang['ut_test_datatype']	= 'ชนิดข้อมูลที่ทดสอบ';
-$lang['ut_res_datatype']	= 'ชนิดข้อมูลที่คาดหวัง';
-$lang['ut_result']			= 'ผลลัพธ์';
-$lang['ut_undefined']		= 'ไม่ได้กำหนดชื่อการทดสอบ';
-$lang['ut_file']			= 'ชื่อไฟล์';
-$lang['ut_line']			= 'หมายเลขบรรทัด';
-$lang['ut_passed']			= 'ผ่าน';
-$lang['ut_failed']			= 'ไม่ผ่าน';
-$lang['ut_boolean']			= 'Boolean';
-$lang['ut_integer']			= 'Integer';
-$lang['ut_float']			= 'Float';
-$lang['ut_double']			= 'Float'; // can be the same as float
-$lang['ut_string']			= 'String';
-$lang['ut_array']			= 'Array';
-$lang['ut_object']			= 'Object';
-$lang['ut_resource']		= 'Resource';
-$lang['ut_null']			= 'Null';
-$lang['ut_notes']			= 'บันทึกเพิ่มเติม';
+$lang['ut_test_name'] = 'ชื่อการทดสอบ';
+$lang['ut_test_datatype'] = 'ชนิดข้อมูลที่ทดสอบ';
+$lang['ut_res_datatype'] = 'ชนิดข้อมูลที่คาดหวัง';
+$lang['ut_result'] = 'ผลลัพธ์';
+$lang['ut_undefined'] = 'ไม่ได้กำหนดชื่อการทดสอบ';
+$lang['ut_file'] = 'ชื่อไฟล์';
+$lang['ut_line'] = 'หมายเลขบรรทัด';
+$lang['ut_passed'] = 'ผ่าน';
+$lang['ut_failed'] = 'ไม่ผ่าน';
+$lang['ut_boolean'] = 'Boolean';
+$lang['ut_integer'] = 'Integer';
+$lang['ut_float'] = 'Float';
+$lang['ut_double'] = 'Float'; // can be the same as float
+$lang['ut_string'] = 'String';
+$lang['ut_array'] = 'Array';
+$lang['ut_object'] = 'Object';
+$lang['ut_resource'] = 'Resource';
+$lang['ut_null'] = 'Null';
+$lang['ut_notes'] = 'หมายเหตุ';
